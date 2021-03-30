5a5eb1bd8350ca795c8d2c9501e9f1059080d72e
Iryna <ira01khomyk@gmail.com>
2021-03-30 16:33:28 +0300

    updated ProjectBackEnd

---
+++ ProjectBackEnd/Gamification/Gamification/Controllers/AchievementController.cs
@@ -0,0 +1,43 @@
+﻿using Gamification.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Gamification.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class AchievementController : ControllerBase
+    {
+        string path = @"./../Gamification/MockData/mock_data.json";
+
+        private ILogger<AchievementController> _logger;
+        List<Achievement> CreatedList;
+        public AchievementController(ILogger<AchievementController> logger)
+        {
+            _logger = logger;
+
+            string readText = System.IO.File.ReadAllText(path);
+            CreatedList = JsonSerializer.Deserialize<List<Achievement>>(readText);
+        }
+
+        [HttpGet]
+        public IEnumerable<Achievement> GetAll()
+        {
+            return CreatedList;
+        }
+
+        [HttpGet("{Id}")]
+        public Achievement GetAchievementById(int Id)
+        {
+
+            Achievement achiv =  CreatedList[CreatedList.FindIndex(elem => elem.Id == Id)];
+            return achiv;
+        }
+    }
+}
