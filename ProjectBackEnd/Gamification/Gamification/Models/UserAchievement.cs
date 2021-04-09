2db14057f1fd8d8c5005357efddcd6b58f7c0824
IraKhomyk <80851018+IraKhomyk@users.noreply.github.com>
2021-04-09 18:19:39 +0300

    created a user entity and related entities

---
+++ ProjectBackEnd/Gamification/Gamification/Models/UserAchievement.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gamification.Models
+{
+    public class UserAchievement
+    {
+        public Guid UserId { get; set; }
+        public User User { get; set; }
+        public Guid AchievementId { get; set; }
+        public Achievement Achievement {get;set;}
+        public DateTime AddedTime { get; set; }
+        public IEnumerable<UserAchievement> UserAchievements { get; set; }
+
+    }
+}
