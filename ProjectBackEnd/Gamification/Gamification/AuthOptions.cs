fe5d164c84c9c97c9d3f4a22226e18c52082b041
IraKhomyk <80851018+IraKhomyk@users.noreply.github.com>
2021-04-21 18:47:18 +0300

    updated AuthOptions

--- ProjectBackEnd/Gamification/Gamification/AuthOptions.cs
+++ ProjectBackEnd/Gamification/Gamification/AuthOptions.cs
@@ -10,10 +10,10 @@ namespace Gamification
     public class AuthOptions
     {
 
-        public const string Issuer = "MyAuthServer"; // издатель токена
-        public const string Audience = "MyAuthClient"; // потребитель токена
-        const string Secret = "mysupersecret_secretkey!123";   // ключ для шифрации
-        public const int TokenLifeTime = 1; // время жизни токена - 1 минута
+        public const string Issuer = "MyAuthServer"; 
+        public const string Audience = "MyAuthClient"; 
+        const string Secret = "mysupersecret_secretkey!123"; 
+        public const int TokenLifeTime = 1; 
         public static SymmetricSecurityKey GetSymmetricSecurityKey()
         {
             return new SymmetricSecurityKey(Encoding.ASCII.GetBytes(Secret));
