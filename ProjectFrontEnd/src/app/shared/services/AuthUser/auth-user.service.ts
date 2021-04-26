b9dd005409a101528d3cbf8d14ee18afc4169fb2
IraKhomyk <80851018+IraKhomyk@users.noreply.github.com>
2021-04-26 22:35:00 +0300

    Delete auth-user.service.ts

--- ProjectFrontEnd/src/app/shared/services/AuthUser/auth-user.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from "@angular/core";
-import { User } from "src/app/modules/auth/models/user";
-
-@Injectable({
-    providedIn: 'root'
-})
-export class AuthUserService {
-    authUser: User;
-}
